2e8ece02fb47a72d08bb7929f569a5210005c1ab
Ringo Leese <r.leese@locr.com>
2024-06-17 14:18:07 +0200

    improvements by SonarCloud hints

--- src/CsvReader.php
+++ src/CsvReader.php
@@ -186,9 +186,10 @@ class CsvReader extends BaseTableReader
         foreach ($output as $line) {
             if (preg_match($validFileRegex, $line, $lineMatch)) {
                 $validLineFound = true;
-                if (isset($lineMatch[3]) && trim($lineMatch[3]) === '(with BOM)') {
-                    $withBOM = true;
-                } elseif (isset($lineMatch[5]) && trim($lineMatch[5]) === '(with BOM)') {
+                if (
+                    (isset($lineMatch[3]) && trim($lineMatch[3]) === '(with BOM)') ||
+                    (isset($lineMatch[5]) && trim($lineMatch[5]) === '(with BOM)')
+                ) {
                     $withBOM = true;
                 }
                 break;
